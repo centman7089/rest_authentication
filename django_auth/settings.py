94f5ea86f6a18374e2a5498dcb0a8258599c6ea4
Centman7089 <innocentimitini@gmail.com>
2025-08-27 13:31:39 +0100

    add

--- django_auth/settings.py
+++ django_auth/settings.py
@@ -113,7 +113,8 @@ USE_TZ = True
 
 # Static & Media files
 STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR/"static"
+# STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
